1edbf0d46e44747c0d78bd25a02c8372efd9e519
desarrollo-genios <166955572+desarrollo-genios@users.noreply.github.com>
2024-04-17 18:40:45 -0500

    Add files via upload

---
+++ eliminar.php
@@ -0,0 +1,13 @@
+<?php
+$documento = $_GET['documento'];
+$db = new PDO('mysql:host=localhost;dbname=cf41','root','');
+$db->query("DELETE FROM clientes WHERE documento='$documento';");
+
+header("location:listar.php");
+
+
+
+?>
+
+
+
---
+++ listar.php
@@ -0,0 +1,90 @@
+<?php
+$db = new PDO('mysql:host=localhost;dbname=cf41','root','');
+$clientes = $db->query("SELECT * FROM clientes")->fetchAll(PDO::FETCH_OBJ);
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+</head>
+<body>
+    <div class="container">
+        <table class="table table-hover table-secondary">
+            <tr>
+                <th>Documento</th>
+                <th>Nombre</th>
+                <th>Sexo</th>
+                <th>Edad</th>
+                <th>Opciones</th>
+            </tr>
+            <?php foreach($clientes as $cliente):?>
+                <tr>
+                    <td><?php echo $cliente->documento;?></td>
+                    <td><?php echo $cliente->nombre;?></td>
+                    <td><?php echo $cliente->sexo;?></td>
+                    <td><?php echo $cliente->edad;?></td>
+                    <td>
+                        <a href="eliminar.php?documento=<?php echo $cliente->documento;?>">
+                            <i class="bi bi-trash"></i>
+                        </a>
+                    </td>
+                </tr>
+            <?php endforeach;?>
+        </table>
+        <a href="formulario.html" class="btn btn-success">Nuevo Cliente</a>
+    </div>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+/*
+foreach ($clientes as $cliente):
+    echo $cliente->nombre . " " . $cliente->sexo . "<br>";
+endforeach;
+*/
+?>
